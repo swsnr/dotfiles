68321e62449d827cb8d5945b05c7600fee44a618
Sebastian Wiesner <sebastian@swsnr.de>
2018-06-17 13:46:55 +0200

    Update update function :)

--- fish/functions/update-everything.fish
+++ fish/functions/update-everything.fish
@@ -1,10 +1,12 @@
 # CC0, see <http://creativecommons.org/publicdomain/zero/1.0/>
 
 function update-everything -d "Update all my stuff"
-    pushd ~/GitHub/playbooks
-    echo -s (set_color -o) 'Updating playbooks' (set_color normal)
-    git pull --rebase --autostash
-    echo -s (set_color -o) 'Updating system via ansible playbooks' (set_color normal)
-    ansible-playbook -t update -l (hostname -s) $argv site.yml
-    popd
+    echo -s (set_color -o) 'Updating homebrew' (set_color normal)
+    if string match --quiet 'darwin*' $OSTYPE
+        brew upgrade
+    end
+    echo -s (set_color -o) 'Update Rust packages' (set_color normal)
+    cargo install-update --all
+    echo -s (set_color -o) 'Update all Texlive packages' (set_color normal)
+    sudo tlmgr update --self --all
 end
