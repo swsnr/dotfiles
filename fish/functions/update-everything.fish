d5f3d7707175198b3a9ea0151713ed8c8aecd0b9
Sebastian Wiesner <sebastian@swsnr.de>
2018-09-17 09:56:43 +0200

    Guard things in update-everything

--- fish/functions/update-everything.fish
+++ fish/functions/update-everything.fish
@@ -1,12 +1,16 @@
 # CC0, see <http://creativecommons.org/publicdomain/zero/1.0/>
 
 function update-everything -d "Update all my stuff"
-    echo -s (set_color -o) 'Updating homebrew' (set_color normal)
     if string match --quiet 'darwin*' $OSTYPE
+        echo -s (set_color -o) 'Updating homebrew' (set_color normal)
         brew upgrade
     end
+
     echo -s (set_color -o) 'Update Rust packages' (set_color normal)
     cargo install-update --all
-    echo -s (set_color -o) 'Update all Texlive packages' (set_color normal)
-    sudo tlmgr update --self --all
+
+    if command --search tlmgr >/dev/null
+        echo -s (set_color -o) 'Update all Texlive packages' (set_color normal)
+        sudo tlmgr update --self --all
+    end
 end
