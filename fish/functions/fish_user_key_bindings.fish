87d632b77d67b3382bf380a6e4c98c9012f9e16d
Sebastian Wiesner <sebastian@swsnr.de>
2018-10-05 13:14:42 +0200

    Prepend sudo to command line

---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,19 @@
+# Copyright 2018 Sebastian Wiesner <sebastian@swsnr.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the spec
+
+function fish_user_key_bindings
+    fish_default_key_bindings
+
+    # Prepend sudo to the command line with M-s
+    bind \es sudo-commandline
+end
