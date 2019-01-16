8b8d0f1cff44f4be81f8b4d274c7d3c52d28b43b
Sebastian Wiesner <sebastian@swsnr.de>
2019-01-16 09:53:59 +0100

    Load autojump from Linuxbrw

--- fish/conf.d/autojump.fish
+++ fish/conf.d/autojump.fish
@@ -14,7 +14,10 @@
 
 # Autojump for fast directory jumping
 if status --is-interactive
-    for directory in "$HOME/.autojump" '/usr/local' '/usr'
+    for directory in "$HOME/.autojump/" \
+        '/home/linuxbrew/.linuxbrew/' \
+        '/usr/local/' \
+        '/usr/'
         set -l __autojump_file "$directory/share/autojump/autojump.fish"
         if [ -f $__autojump_file ]
             source $__autojump_file
