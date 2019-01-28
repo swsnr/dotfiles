8f83c3bba43289308b62cfd21e3a11aa6e73cfdb
Sebastian Wiesner <sebastian@swsnr.de>
2019-01-28 13:35:06 +0100

    Relicense albert plugin
    Albert is GPL apparently the extension needs to be as well.

--- albert/extensions/isodatetime.py
+++ albert/extensions/isodatetime.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 019 Sebastian Wiesner <sebastian@swsnr.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Copy ISO date to clipboard
 
 """
@@ -7,7 +22,7 @@
 __iid__ = "PythonInterface/v0.2"
 __prettyname__ = "ISO Date"
 __version__ = "1.1"
-__trigger__ = "isodt"
+__trigger__ = "isodt "
 __author__ = "Sebastian Wiesner"
 __dependencies__ = []
 
