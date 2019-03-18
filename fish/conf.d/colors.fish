0cec8eec45ec72c34e0c4c58c303500a4b808333
Sebastian Wiesner <sebastian@swsnr.de>
2019-03-18 11:27:42 +0100

    Fix typo

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -14,7 +14,7 @@
 
 if status --is-interactive
     # See https://github.com/lunaryorn/term-background.rs
-    if command --search 'term-background' >/dev/nullz
+    if command --search 'term-background' >/dev/null
         set LY_TERM_BACKGROUND (term-background (tty))
         if string match -q light $LY_TERM_BACKGROUND
             set -x BAT_THEME OneHalfLight
