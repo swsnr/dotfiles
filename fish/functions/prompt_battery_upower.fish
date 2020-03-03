7435cc61e6c3e5c4ccf91a7e24b285d5034c0344
Sebastian Wiesner <sebastian@swsnr.de>
2020-03-03 09:50:06 +0100

    Continue upower battery prompt

--- fish/functions/prompt_battery_upower.fish
+++ fish/functions/prompt_battery_upower.fish
@@ -31,57 +31,29 @@ function prompt_battery_upower -d 'upower battery info in prompt'
     # whole regex, so we start at 2 for the 1st matching group.
     set -l percentage (string match -r '^percentage:\s+(.+)' $battery_info)[2]
     set -l state (string match -r '^state:\s+(.+)' $battery_info)[2]
+    set -l remaining (string match -r '^time to empty:\s+(.+)' $battery_info)[2]
+    set -l level
 
-    # TODO: Figure out remaining time and state values
-    set -l remaining $matches[4]
+    # TODO: Time to charge?
 
-    # Try to parse the actual hours and minutes out of the remaining time
-    # estimate to include them in the prompt. Fall back to the precentage
-    # level if the system can't provide any estimation of the remaining time
-    set -l time (string match -r '\\d+:\\d+' $remaining)
-    if test (count $time) -gt 0
-        set level "|$time[1]"
+    if test -n "$remaining"
+        set level "|$remaining"
     else
-        set level "|$percentage%"
+        set level "|$percentage"
     end
 
     # Parse the state into a colour to use in the prompt and a state symbol
     # to indicate the battery state.
     switch $state
-        case 'charged'
+        case 'fully-charged'
             # Bail out if the battery is fully charged. In this case battery
             # information is really redundant, and we do not want to show
             # anything.
             return 0
-        case 'finishing charge'
-            # Likewise if the battery is about to be fully charged any moment
-            return 0
-        case 'AC attached'
-            # A special state when the AC adapter is freshly plugged but macOS
-            # hasn't yet started to charge the battery
-            set colour (set_color 'green')
-            set state_symbol '-'
-        case 'charging'
-            set colour (set_color 'green')
-            set state_symbol '↑'
         case 'discharging'
-            # When the battery is discharging check the output again for
-            # a potential warning about low battery level
-            set -l matches (string match -r 'Battery Warning: (\\S+)' $battery_status)
-            if test (count $matches) -gt 0
-                switch $matches[2]
-                    case 'Early'
-                        set state_symbol '↡'
-                        set colour (set_color 'red')
-                    case '*'
-                        # "Final" or something maybe even worse \o/
-                        set state_symbol '↡'
-                        set colour (set_color -b 'red' -o 'white')
-                end
-            else
-                set state_symbol '↓'
-                set colour (set_color 'yellow')
-            end
+            # TODO: extract warning-level
+            set state_symbol '↓'
+            set colour (set_color 'yellow')
         case *
             echo -s -n (set_color -o red) 'UNKNOWN STATE:' $state (set_color normal)
             return 1
