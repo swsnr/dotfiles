fc8faf51053a8d21ac095cf1bb6ac2c9ebf8dc4e
Sebastian Wiesner <sebastian@swsnr.de>
2020-03-05 15:55:42 +0100

    Update texts for locker

--- linux/i3/bin/lock-screen.bash
+++ linux/i3/bin/lock-screen.bash
@@ -18,12 +18,12 @@ systemctl --user kill --signal=USR1 dunst
 
 # Do not fork because we need to wait for unlock, for xss-lock as well asfor
 # re-enabling dunst.  Blur all windows
-i3lock -B6 -n \
+i3lock -B6 -n --radius=30 \
     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
-    --radius=30 --veriftext="🌀" --wrongtext="❌"
+    --veriftext="" --wrongtext="" --noinputtext="_"
 
 # Resume dunst after unlock
 systemctl --user kill --signal=USR2 dunst
