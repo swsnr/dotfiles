aa22d34bb76b1ff2d413fdb658d61a5d699ed209
Sebastian Wiesner <sebastian@swsnr.de>
2020-04-13 14:52:51 +0200

    Add calendar to prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -58,6 +58,10 @@ function fish_prompt -d 'My personal prompt'
 
     # Time
     echo -sn (set_color -o) ' at ' (set_color -o cyan) (date '+%H:%M') (set_color normal)
+    if command -q wcal && test 120 -le $COLUMNS
+        # And a small week calender if terminal size permits
+        echo -sn ' (' (wcal -cC | tail -n1) ')'
+    end
 
     # Battery if present and supported
     set -l battery (prompt_battery)
