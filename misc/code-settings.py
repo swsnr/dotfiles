05e5c714dfb722e151b2877f1d92fff16640ed8d
Sebastian Wiesner <sebastian@swsnr.de>
2020-04-15 08:36:12 +0200

    Keep track of code extensions

--- misc/code-settings.py
+++ misc/code-settings.py
@@ -14,6 +14,7 @@
 # the License.
 
 import json
+from subprocess import run
 from pathlib import Path
 
 
@@ -41,7 +42,28 @@ SETTINGS = {
 }
 
 
-def main():
+CODE_EXTENSIONS = [
+    # Editing
+    'stkb.rewrap',
+    # Misc formats
+    'coolbear.systemd-unit-file',
+    # Fish editing and formatting
+    'bmalehorn.vscode-fish',
+    # Shell scripts
+    'timonwong.shellcheck',
+    'foxundermoon.shell-format',
+    # Rust and TOML
+    'bungcip.better-toml',
+    'rust-lang.rust',
+    'serayuzgur.crates',
+    # Languages
+    'James-Yu.latex-workshop',
+    'ms-python.python',
+    'vscoss.vscode-ansible',
+]
+
+
+def update_config():
     config_file = Path.home() / '.config' / 'Code - OSS' / 'User' / 'settings.json'
     settings = {}
     if config_file.is_file():
@@ -50,6 +72,22 @@ def main():
     settings.update(SETTINGS)
     with config_file.open('w') as sink:
         json.dump(settings, sink, indent=4)
+
+
+def install_extensions():
+    installed_extensions = set(
+        run(['code', '--list-extensions'],
+            capture_output=True,
+            check=True,
+            text=True).stdout.splitlines())
+    for extension in CODE_EXTENSIONS:
+        if extension not in installed_extensions:
+            run(['code', '--install-extension', extension], check=True)
+
+
+def main():
+    update_config()
+    install_extensions()
 
 
 if __name__ == '__main__':
