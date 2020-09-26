a818df27421e52ad83b904aa4be7aa5b8743380a
Sebastian Wiesner <sebastian@swsnr.de>
2020-09-26 20:37:32 +0200

    Cleanup i3 stuff

--- x11/xprofile.sh
+++ x11/xprofile.sh
@@ -16,25 +16,15 @@
 # Import environment from fish
 eval "$(fish -l -c dump_env_posix)"
 
-if [[ "${XDG_CURRENT_DESKTOP}" == i3 && "$(lsb_release -is)" == "Ubuntu" ]]; then
-    # Cheat a little to treat a Ubuntu i3 desktop as Ubuntu system
-    export XDG_CURRENT_DESKTOP=i3:ubuntu
-fi
-
-if [[ "${XDG_CURRENT_DESKTOP}" == i3* ]]; then
-    # Point dconf to a custom profile pointing to a custom database;
-    # this separates config changes made for i3 from changes made in Gnome sessions
-    export DCONF_PROFILE="$HOME/.config/i3/dconf-profile"
-
-    # Enable i3 theme
+case "${XDG_CURRENT_DESKTOP}" in
+    *i3*)
+      # Enable i3 theme
     i3_theme="$HOME/.config/i3/themes/current"
     if [ -f "$i3_theme" ]; then
         xrdb -merge "$i3_theme"
     fi
 
     # Restore screen layout
-    autorandr --change --default horizontal &>/dev/null
-fi
-
-# Make Qt5 apps use qt5ct
-#export QT_QPA_PLATFORMTHEME=qt5ct
+    autorandr --change --default horizontal >/dev/null 2>&1
+    ;;
+esac
