730aba5f50b0f2f81fcf63e851c42ed348c9df47
Sebastian Wiesner <sebastian@swsnr.de>
2021-12-01 14:17:40 +0100

    Update backup repo path

--- backup/backup.bash
+++ backup/backup.bash
@@ -15,7 +15,7 @@
 
 set -xeuo pipefail
 
-exec restic -r sftp:basti@kastl.local:restic backup ~ \
+exec restic -r "sftp:$USERNAME@kastl.local:restic/$USERNAME" backup ~ \
     --one-file-system \
     --tag basti \
     --tag dotfiles-script \
