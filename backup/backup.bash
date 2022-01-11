553fca3f3f01dc5983039d6415037c06cffd031c
Sebastian Wiesner <sebastian@swsnr.de>
2022-01-11 20:48:38 +0100

    Fix inhibition during backup
    Use gnome-session-inhibit and pass the app ID of the terminal running
    the backup, to make the inhibitor appear in the Gnome logout dialog.
    systemd inhibitors can block shutdown, but Gnome session logout never
    shows them properly.
    
    See https://discourse.gnome.org/t/gnome-logout-dialog-ignores-inhibitors/8602/4
    and following posts for details.

--- backup/backup.bash
+++ backup/backup.bash
@@ -15,7 +15,30 @@
 
 set -xeuo pipefail
 
-exec systemd-inhibit --who backup.bash --why='Ongoing backup' \
+INHIBITOR_APP_ID=""
+
+if [[ -n ${GNOME_TERMINAL_SERVICE:-} ]]; then
+    INHIBITOR_APP_ID=org.gnome.Terminal.desktop
+fi
+
+if [[ -z "$INHIBITOR_APP_ID" ]]; then
+    echo 'Cannot determine terminal App to inhibit logout during backup'
+    exit 1
+fi
+
+# Inhibit logout (which includes shutdown) and suspend in the name of the
+# running terminal emulator, to make sure the backup completes without
+# interruptions.
+#
+# We need to use gnome-session-inhibit and an existing app ID to make the
+# inhibitor appear in the logout dialog; Gnome session logout doesn't show
+# systemd inhibitors and it doesn't show inhibitors without app IDs, because no
+# icon I guess.  See https://discourse.gnome.org/t/gnome-logout-dialog-ignores-inhibitors/8602/4
+# for details; not the most useful behaviour in my opinion, but it's not a fight
+# for me to pick.
+exec gnome-session-inhibit \
+    --app-id "$INHIBITOR_APP_ID" --reason 'Ongoing backup' \
+    --inhibit "logout:suspend" \
     restic -r "rclone:kastl:restic-$USERNAME" backup ~ \
     --one-file-system \
     --tag basti \
