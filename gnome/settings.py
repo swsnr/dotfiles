11da9d7d2e7a983de081d67f8ac0a8eb3b0d4acf
Sebastian Wiesner <sebastian@swsnr.de>
2022-01-31 11:40:33 +0100

    Drop dotbot

---
+++ gnome/settings.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+# Copyright Sebastian Wiesner <sebastian@swsnr.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import sys
+from gi.repository import Gio
+
+SETTINGS = {
+    'org.gnome.desktop.interface': {
+        # Disable hot corner
+        'enable-hot-corners': False,
+        # Show date and weekday in clock
+        'clock-show-date': True,
+        'clock-show-weekday': True,
+        # Disable Ctrl shortcut for locating the cursor; conflicts with
+        # IntelliJs Ctrl shortcut to add multiple cursors
+        'locate-pointer': False
+    },
+    'org.gnome.shell.app-switcher': {
+        # Limit app and window switcher to current workspace"""  """
+        'current-workspace-only': True
+    },
+    'org.gnome.system.location': {
+        # Enable location service
+        'enabled': True
+    },
+    'org.gnome.desktop.peripherals.keyboard': {
+        'remember-numlock-state': False,
+        'numlock-state': True
+    },
+    'org.gnome.desktop.input-sources': {
+        # Set compose key according to my moonlander layout
+        'xkb-options': ['compose:ralt']
+    },
+    'org.gnome.desktop.calendar': {
+        # Show week numbers in calendar
+        'show-weekdate': True
+    },
+    'org.gnome.mutter': {
+        'dynamic-workspaces': True
+    },
+    'org.gnome.desktop.wm.keybindings': {
+        'close': ['<Super>q', '<Alt>F4'],
+        # Switch windows not applications
+        'switch-applications': [],
+        'switch-applications-backward': [],
+        'switch-group': [],
+        'switch-group-backward': [],
+        'switch-windows': ['<Super>Tab', '<Alt>Tab'],
+        'switch-windows-backward': ['<Shift><Super>Tab', '<Shift><Alt>Tab'],
+        # Keep on top above all other windows
+        'always-on-top': ['<Shift><Super>t'],
+        'toggle-on-all-workspaces': ['<Super>period'],
+        # Disable input method switching wtih Super+Space
+        'switch-input-source': ['XF86Keyboard'],
+        'switch-input-source-backward': ['<Shift>XF86Keyboard'],
+        # Toogle fullscreen
+        'toggle-fullscreen': ['<Super>Return']
+    }
+}
+
+TERMINAL_PROFILE = {
+    'audible-bell': False,
+    'bold-is-bright': False,
+    'default-size-columns': 120,
+    'default-size-rows': 40,
+    'font': 'PragmataPro Mono Liga 11',
+    'use-system-font': False,
+    'visible-name': 'Shell'
+}
+
+BINDINGS = {
+    'terminal': False,
+    'toggle-theme': {
+        'name': 'Toggle UI theme',
+        'command': 'ui-theme toggle',
+        'binding': '<Super>apostrophe'
+    },
+    'onepassword-quick': {
+        'name': '1Password quick access',
+        'command': '1password --quick-access',
+        'binding': '<Super>o'
+    }
+}
+
+
+def set_pytype(settings, key, value):
+    if isinstance(value, str):
+        settings.set_string(key, value)
+    elif isinstance(value, bool):
+        settings.set_boolean(key, value)
+    elif isinstance(value, int):
+        settings.set_int(key, value)
+    elif isinstance(value, list):
+        settings.set_strv(key, value)
+    else:
+        raise ValueError(f'Value {value!r} for key {key} has unknown type')
+
+
+def apply_settings(settings, items):
+    settings_schema = settings.get_property('settings-schema')
+    schema = settings.get_property('schema')
+    for key, value in items.items():
+        if settings_schema.has_key(key):
+            print(f'{schema}.{key} = {value}')
+            set_pytype(settings, key, value)
+        else:
+            print(f'{schema}.{key} does not exist!', file=sys.stderr)
+
+
+def main():
+    source = Gio.SettingsSchemaSource.get_default()
+    for schema, items in SETTINGS.items():
+        if source.lookup(schema, False):
+            settings = Gio.Settings(schema=schema)
+            apply_settings(settings, items)
+        else:
+            print(f'Skipping non-existing schema {schema}', file=sys.stderr)
+
+    if not source.lookup('org.gnome.Terminal.ProfilesList', False):
+        print('Terminal profile list not available, skipping', file=sys.stderr)
+    else:
+        profiles_list = Gio.Settings(
+            schema='org.gnome.Terminal.ProfilesList')
+        profile_id = profiles_list.get_string('default')
+        schema = 'org.gnome.Terminal.Legacy.Profile'
+        path = f'/org/gnome/terminal/legacy/profiles:/:{profile_id}/'
+        settings = Gio.Settings.new_with_path(schema_id=schema, path=path)
+        apply_settings(settings, TERMINAL_PROFILE)
+
+    bindings_schema = 'org.gnome.settings-daemon.plugins.media-keys.custom-keybinding'
+    if not source.lookup(bindings_schema, False):
+        print('Schema for custom keybindings not found, skipping', file=sys.stderr)
+    else:
+        new_bindings = []
+        removed_bindings = []
+        for id, binding in BINDINGS.items():
+            path = f'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/{id}/'
+            settings = Gio.Settings.new_with_path(
+                schema_id=bindings_schema, path=path)
+            if not binding:
+                settings_schema = settings.get_property('settings-schema')
+                for key in settings_schema.list_keys():
+                    settings.reset(key)
+                print(f'binding {id} removed')
+                removed_bindings.append(path)
+            else:
+                settings.set_string('name', binding['name'])
+                settings.set_string('command', binding['command'])
+                settings.set_string('binding', binding['binding'])
+                print('{id} {binding}: {name} ({command})'.format(
+                    id=id, **binding))
+                new_bindings.append(path)
+
+        media_keys = Gio.Settings(
+            schema='org.gnome.settings-daemon.plugins.media-keys')
+        custom_bindings = media_keys.get_strv('custom-keybindings')
+        for path in new_bindings:
+            if path not in custom_bindings:
+                custom_bindings.append(path)
+        for path in removed_bindings:
+            if path in custom_bindings:
+                custom_bindings.remove(path)
+        media_keys.set_strv('custom-keybindings', custom_bindings)
+
+
+if __name__ == '__main__':
+    main()
--- install.bash
+++ install.bash
@@ -1,17 +1,81 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Copyright Sebastian Wiesner <sebastian@swsnr.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
-set -e
+set -xeuo pipefail
 
-CONFIG="install.conf.yaml"
-DOTBOT_DIR=".dotbot/dotbot"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")"  >/dev/null 2>&1 && pwd)"
 
-DOTBOT_BIN="bin/dotbot"
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Binaries
+mkdir -p ~/.local/bin
+ln -fs -t ~/.local/bin/ "$DIR/bin/"*
 
-cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${DOTBOT_DIR}"
+# Fish shell config files and functions
+mkdir -p ~/.config/fish/functions
+ln -fs -t ~/.config/fish "$DIR/fish/config.fish" "$DIR/fish/fish_plugins"
+ln -fs -t ~/.config/fish/functions "$DIR/fish/functions/"*.fish
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" \
-    -p ./.dotbot/plugins/dotbot-gsettings/gsettings.py \
-    -c "${CONFIG}" "${@}"
+# Vim
+ln -fs "$DIR/vim/ideavimrc" ~/.ideavimrc
+mkdir -p ~/.config/nvim/after
+mkdir -p ~/.local/share/nvim/site/pack/dotfiles/start
+ln -fs "$DIR/vim/init.vim" ~/.config/nvim/init.vim
+ln -fs -t ~/.config/nvim/after "$DIR/vim/after/"*
+# Vim plugins
+for plugin in vim-toml vim-fish; do
+    ln -fs -T "$DIR/submodules/nvim/$plugin" "$HOME/.local/share/nvim/site/pack/dotfiles/start/$plugin"
+done
+
+# Git configuration
+mkdir -p ~/.config/git
+ln -fs -t ~/.config/git "$DIR/git/common/"*
+ln -fs -t ~/.config/git "$DIR/git/config.linux"
+
+# SSH configuration
+mkdir -p ~/.ssh
+mkdir -p ~/.ssh/{config.d,known-hosts}
+chmod 0700 ~/.ssh ~/.ssh/{config.d,known-hosts}
+ln -fs "$DIR/ssh/config" ~/.ssh/config
+ln -fs -t ~/.ssh/known-hosts "$DIR/ssh/known-hosts/"*
+ln -fs -t ~/.ssh/config.d "$DIR/ssh/config.d/"*
+
+# Scala configuration
+mkdir -p ~/.ammonite ~/sbt/1.0/plugins/project
+ln -fs "$DIR/scala/ammonite-predef.sc" ~/.ammonite/predef.sc
+ln -fs "$DIR/scala/settings.sbt" ~/.sbt/1.0/settings.sbt
+ln -fs -t ~/.sbt/1.0/plugins "$DIR/scala/"{plugins,sbt-updates}.sbt
+ln -fs -t ~/.sbt/1.0/plugins/project/ "$DIR/scala/sbt-updates.sbt"
+
+# Misc files
+mkdir -p ~/.config/{bat,latexmk,wezterm,restic}
+ln -fs "$DIR/latex/latexmkrc" ~/.config/latexmk/latexmkrc
+ln -fs "$DIR/misc/bat" ~/.config/bat/config
+ln -fs "$DIR/misc/wezterm.lua" ~/.config/wezterm/wezterm.lua
+ln -fs "$DIR/backup/linux.exclude" ~/.config/restic/linux.exclude
+ln -fs "$DIR/misc/XCompose" ~/.XCompose
+
+# Gnome
+mkdir -p ~/.local/share/gnome-shell/extensions
+ln -fs -t ~/.local/share/gnome-shell/extensions "$DIR/gnome/extensions/home@swsnr.de"
+"$DIR/gnome/settings.py"
+
+# Generate additional fish completions
+mkdir -p ~/.config/fish/completions
+command -v rclone >& /dev/null &&
+    rclone completion fish > ~/.config/fish/completions/rclone.fish
+command -v restic >& /dev/null &&
+    restic generate --fish-completion ~/.config/fish/completions/restic.fish
+
+# TODO: Drop fisher in favourite of git subtrees
+fish -c 'type -q fisher || begin; curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher; end'
