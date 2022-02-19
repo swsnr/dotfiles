adeba1acd465be1081c884b74f73b69857034b0b
Sebastian Wiesner <sebastian@swsnr.de>
2022-02-19 13:16:58 +0100

    Update wezterm configuration

--- misc/wezterm.lua
+++ misc/wezterm.lua
@@ -7,45 +7,13 @@ function font_with_fallback(name, params)
 end
 
 return {
-  -- color_scheme = "Builtin Tango Light",
-  -- color_scheme = "OneHalfLight",
-  color_scheme = "Tomorrow",
+  color_scheme = "Dracula",
   font = font_with_fallback('PragmataPro Mono Liga'),
-  -- For some reason we need to explicitly and unambiguously specify variants
-  -- of PragmataPro Mono Liga here; wezterm fails to find the italic and bold
-  -- variants properly.
-  font_rules = {
-    {
-      italic = false,
-      intensity = "Normal",
-      font = font_with_fallback('PragmataProMonoLiga-Regular')
-    },
-    {
-      italic = false,
-      intensity = "Normal",
-      font = font_with_fallback('PragmataProMonoLiga-Regular')
-    },
-    {
-      italic = true,
-      intensity = "Normal",
-      font = font_with_fallback('PragmataProMonoLiga-Italic')
-    },
-    {
-      italic = true,
-      intensity = "Half",
-      font = font_with_fallback('PragmataProMonoLiga-Italic')
-    },
-    {
-      italic = true,
-      intensity = "Bold",
-      font = font_with_fallback('PragmataProMonoLiga-BoldItalic')
-    }
-  },
   font_size = 11.0,
   initial_rows = 40,
   initial_cols = 120,
   enable_wayland = true,
-  -- Subpixel rendering!
-  freetype_load_target = "HorizontalLcd",
-  freetype_render_target = "HorizontalLcd",
+  -- Scrollback
+  scrollback_lines = 10000,
+  enable_scroll_bar = true,
 }
