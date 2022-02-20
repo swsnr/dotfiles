062e6a07cb976a129d419154f12f6e664fda39ef
Sebastian Wiesner <sebastian@swsnr.de>
2022-02-20 15:48:30 +0100

    Decrease update time

--- neovim/init.lua
+++ neovim/init.lua
@@ -33,6 +33,7 @@ vim.opt.guifont = 'JetBrains Mono,Note Color Emoji:h11'
 vim.opt.mouse = 'nv' -- Enable mouse in normal and visual mode
 
 -- Options for the general user interface
+vim.opt.updatetime = 300 -- Update faster
 vim.opt.showmode = false -- Don't show mode message in message line
 vim.opt.signcolumn = 'yes' -- Always show sign column
 vim.opt.timeoutlen = 500 -- Key timeout after 500, for which key
