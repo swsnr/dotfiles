229b4a3c6be3865a769df96a5f66045dbd1a5d32
Sebastian Wiesner <sebastian@swsnr.de>
2022-02-20 10:00:53 +0100

    Tune wezterm configuration

--- misc/wezterm.lua
+++ misc/wezterm.lua
@@ -10,10 +10,24 @@ return {
   color_scheme = "Dracula",
   font = font_with_fallback('PragmataPro Liga'),
   font_size = 12.0,
+  -- Default initial window size
   initial_rows = 40,
   initial_cols = 120,
+  -- Run on native wayland by default
   enable_wayland = true,
   -- Scrollback
   scrollback_lines = 10000,
   enable_scroll_bar = true,
+  -- Give us the latest unicode, to make emojis work well on my local systems.
+  -- Probably breaks SSH'ing into some old servers but then again these likely
+  -- won't use emojis anyway.
+  unicode_version = 14,
+  -- Don't beep
+  audible_bell = 'Disabled',
+  visual_bell = {
+    fade_in_function = "EaseIn",
+    fade_in_duration_ms = 150,
+    fade_out_function = "EaseOut",
+    fade_out_duration_ms = 150,
+  },
 }
