909d2f1ecff798882ad8ee96b32fca31452341bd
Sebastian Wiesner <sebastian@swsnr.de>
2022-02-20 18:52:28 +0100

    Add nvim tree as file explorer

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -453,6 +453,20 @@ return packer.startup(function(use)
     end
   }
 
+  -- File explorer sidebar: https://github.com/kyazdani42/nvim-tree.lua
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require'nvim-tree'.setup {}
+
+      require('which-key').register {
+        ['<leader>ft'] = {'<cmd>NvimTreeFindFileToggle<cr>', 'Show current file in tre,e'},
+        ['<leader>fT'] = {'<cmd>NvimTreeFocus<cr>', 'Open file explorer'},
+      }
+    end
+  }
+
   -- Fuzzy finder: https://github.com/nvim-telescope/telescope.nvim
   use {
     'nvim-telescope/telescope.nvim',
