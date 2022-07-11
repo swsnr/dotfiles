d8e8f4796823d22114d22321a7f0f9bae823c573
Sebastian Wiesner <sebastian@swsnr.de>
2022-07-11 16:15:47 +0200

    Open customizable URI on click

--- gnome/extensions/spacetimeformats@swsnr.de/extension.js
+++ gnome/extensions/spacetimeformats@swsnr.de/extension.js
@@ -21,9 +21,11 @@
 const { St, GLib, GObject, Gio, Clutter, GnomeDesktop } = imports.gi;
 
 const Main = imports.ui.main;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
+const ExtensionUtils = imports.misc.extensionUtils;
 const PanelMenu = imports.ui.panelMenu;
 const Util = imports.misc.util;
+
+const Me = ExtensionUtils.getCurrentExtension();
 
 const l = message => log(`${Me.metadata.uuid}: ${message}`);
 
@@ -31,9 +33,9 @@ const SpaceTimesIndicator = GObject.registerClass(
   { GTypeName: "SpaceTimesIndicator" },
   class SpaceTimesIndicator extends PanelMenu.Button {
     _init() {
-      super._init(0.0, `${Me.metadata.name} Indicator`, false);
-      // There's no point pressing this
-      this.setSensitive(false);
+      super._init(0.0, `${Me.metadata.name} Indicator`, true);
+      this._settings = ExtensionUtils.getSettings();
+      this._settingsChangedId = this._settings.connect('changed', this._onSettingsChanged.bind(this));
 
       this._clock = new GnomeDesktop.WallClock()
       this._clockNotifyId = this._clock.connect('notify::clock', this._updateLabel.bind(this));
@@ -43,13 +45,43 @@ const SpaceTimesIndicator = GObject.registerClass(
       this.add_child(this._label);
 
       this._updateLabel();
+      this._onSettingsChanged();
+    }
+
+    vfunc_event(event) {
+      if (this.menu &&
+        (event.type() == Clutter.EventType.TOUCH_BEGIN ||
+          event.type() == Clutter.EventType.BUTTON_PRESS)) {
+        this._open_uri();
+      }
+
+      return Clutter.EVENT_PROPAGATE;
+    }
+
+    _open_uri() {
+      const uri = this._get_uri_to_open();
+      if (!!uri) {
+        Gio.AppInfo.launch_default_for_uri_async(uri, null, null, null);
+      }
+    }
+
+    _get_uri_to_open() {
+      return this._settings.get_string('uri-to-open-on-click');
     }
 
     _onDestroy() {
       this._label.destroy();
       this._clock.disconnect(this._clockNotifyId);
       this._clock.destroy();
+      this._settings.disconnect(this._settingsChangedId);
+      this._settings.destroy();
       super._onDestroy();
+    }
+
+    _onSettingsChanged() {
+      // Enable clicking the indicator only of a URI was configured
+      this.setSensitive(!!this._get_uri_to_open());
+      this.setSensitive(true);
     }
 
     _updateLabel() {
