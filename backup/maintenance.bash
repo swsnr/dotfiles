d2f6b50b4161f663638b589a5e81be3584dbd65b
Sebastian Wiesner <sebastian@swsnr.de>
2022-08-06 19:16:36 +0200

    Add maintenance script for backups

---
+++ backup/maintenance.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Copyright Sebastian Wiesner <sebastian@swsnr.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+set -xeuo pipefail
+
+INHIBITOR_APP_ID=""
+
+if [[ -n ${GNOME_TERMINAL_SERVICE:-} ]]; then
+    INHIBITOR_APP_ID=org.gnome.Terminal.desktop
+elif [[ ${TERM_PROGRAM} == "WezTerm" ]]; then
+    INHIBITOR_APP_ID=org.wezfurlong.wezterm.desktop
+fi
+
+if [[ -z "$INHIBITOR_APP_ID" ]]; then
+    echo 'Cannot determine terminal App to inhibit logout during backup'
+    exit 1
+fi
+
+inhibit() {
+    # See backup.bash about why we have to use gnome-session-inhibit with an
+    # actual application.
+    gnome-session-inhibit \
+        --app-id "$INHIBITOR_APP_ID" --reason 'Pruning backup' \
+        --inhibit "logout:suspend" \
+        "$@"
+}
+
+# Cleanup snapshots made by the dotfiles script:  Definitely keep the last ten
+# snapshots, and# everything within the last six months.  For the last two years
+# keep montly snapshots, and keep a yearly snapshot for like forever.
+inhibit restic -r "rclone:kastl:restic-$USERNAME" forget \
+    --tag basti,dotfiles-script \
+    --keep-last 10 \
+    --keep-within 6m \
+    --keep-monthly 24 \
+    --keep-yearly 20
+# Prune unused data after deleting snapshots
+inhibit nice restic -r "rclone:kastl:restic-$USERNAME" prune
+# Check that the data is still valid
+inhibit nice restic -r "rclone:kastl:restic-$USERNAME" check --read-data-subset '10%'
