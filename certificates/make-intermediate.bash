014d872a4196aa1f99fc9c3e9ca5345f27038a5d
Sebastian Wiesner <sebastian@swsnr.de>
2022-08-06 14:10:16 +0200

    Rotate intermediate

---
+++ certificates/make-intermediate.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+certname=intermediate-"$1"
+
+keyfile="$(mktemp basti-ca-root.XXX.key)"
+trap '{ rm -f -- "$keyfile"; }' EXIT
+
+op document get 'Basti CA root key' --output="$keyfile"
+step-cli certificate create \
+    "$certname" "$certname".crt "$certname".key \
+  --profile intermediate-ca \
+  --ca ./basti-ca-root.crt --ca-key "./${keyfile}" \
+  --not-after=8766h # Valid for 1 year
+
+
