74616c57bb766f4b5d83e0a6d070708fd5809dfc
Sebastian Wiesner <sebastian@swsnr.de>
2022-08-06 14:17:43 +0200

    Add certificate for NAS

---
+++ certificates/make-leaf.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+intermediate=intermediate-"$1"
+subject="$2"
+shift
+shift
+
+mkdir -p leafs
+step-cli certificate create \
+    "$subject" "leafs/$subject".crt "leafs/$subject".key \
+  --profile leaf \
+  --ca "./$intermediate.crt" --ca-key "./$intermediate.key" \
+  --not-after=4383h \
+  "${@}"
+
