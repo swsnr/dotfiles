d20e26550c30d3e789cc3eea6e301a978d77c039
Sebastian Wiesner <sebastian@swsnr.de>
2022-11-02 11:54:23 +0100

    Install devhelp

--- arch/install.bash
+++ arch/install.bash
@@ -321,6 +321,7 @@ flatpaks=(
     org.libreoffice.LibreOffice # Office suite
     com.github.xournalpp.xournalpp # Hand-writing & notes
     org.gnome.dfeet # DBus inspector
+    org.gnome.Devhelp # Gnome development docs
     com.gitlab.newsflash # Desktop RSS reader
 )
 flatpaks_to_remove=()
