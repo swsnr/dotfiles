707544a4f59f69513848787b4fb06133ea1be649
Sebastian Wiesner <sebastian@swsnr.de>
2022-12-31 14:12:39 +0100

    Close some filetypes with q
    diff --git c/nvim/lua/swsnr/autocommands.lua i/nvim/lua/swsnr/autocommands.lua
    index 9e7c196..1a09db0 100644
    --- c/nvim/lua/swsnr/autocommands.lua
    +++ i/nvim/lua/swsnr/autocommands.lua
    @@ -37,6 +37,26 @@ v.nvim_create_autocmd({ "BufRead" }, {
       command = 'execute "normal! gg" | execute "normal! O" | startinsert',
       group = ly_group,
     })
    +
    +-- Close some file types with q
    +vim.api.nvim_create_autocmd({ "FileType" }, {
    +  pattern = {
    +    "qf",
    +    "help",
    +    "man",
    +    "notify",
    +    "lspinfo",
    +    "spectre_panel",
    +    "startuptime",
    +    "tsplayground",
    +    "PlenaryTestPopup",
    +  },
    +  callback = function(event)
    +    vim.bo[event.buf].buflisted = false
    +    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
    +  end,
    +})
    +
     -- Update indentation settings for fish shell and bash
     v.nvim_create_autocmd({ "FileType" }, {
       pattern = { "fish", "sh" },

--- nvim/lua/swsnr/autocommands.lua
+++ nvim/lua/swsnr/autocommands.lua
@@ -37,6 +37,26 @@ v.nvim_create_autocmd({ "BufRead" }, {
   command = 'execute "normal! gg" | execute "normal! O" | startinsert',
   group = ly_group,
 })
+
+-- Close some file types with q
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "lspinfo",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "PlenaryTestPopup",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
 -- Update indentation settings for fish shell and bash
 v.nvim_create_autocmd({ "FileType" }, {
   pattern = { "fish", "sh" },
