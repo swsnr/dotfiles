bd367c7133a4078cdb24086916e6648233a7e92f
Sebastian Wiesner <sebastian@swsnr.de>
2023-01-05 13:33:43 +0100

    Close fugitive with q

--- nvim/lua/swsnr/autocommands.lua
+++ nvim/lua/swsnr/autocommands.lua
@@ -42,6 +42,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     "startuptime",
     "tsplayground",
     "PlenaryTestPopup",
+    "fugitive",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
