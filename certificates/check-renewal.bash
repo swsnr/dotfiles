37b897869a26ea7bbcf915bc9c982f022e8fcdea
Sebastian Wiesner <sebastian@swsnr.de>
2023-03-11 13:24:09 +0100

    Remove self-signed certificates

--- certificates/check-renewal.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -euo pipefail
-
-for certificate in **/*.crt; do
-    if step-cli certificate needs-renewal "$certificate"; then
-        echo "$certificate"
-    fi
-done
--- certificates/make-intermediate.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -euo pipefail
-
-step-cli certificate create \
-    "$HOSTNAME Intermediate" "$HOSTNAME-intermediate.crt" "$HOSTNAME-intermediate".key \
-    --profile intermediate-ca \
-    --ca ./swsnr.crt --ca-key ./swsnr.key \
-    --not-after=17532h # Valid for two years
