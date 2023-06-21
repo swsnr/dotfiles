e0b8bdad2267d348e9f037e02bfcefc60a3cc13b
Sebastian Wiesner <sebastian@swsnr.de>
2023-06-21 10:52:41 +0200

    Install flameshot

--- arch/install.bash
+++ arch/install.bash
@@ -527,6 +527,7 @@ case "$HOSTNAME" in
         evolution-ews      # Exchange for evolution
         mattermost-desktop # Chat application
         rocketchat-desktop # Chat application
+        flameshot          # Powerful screenshot application
     )
 
     packages_to_install_optdeps+=(
