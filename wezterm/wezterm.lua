024aa9a0cb19d948e898b3493d1c8ab625f856a5
Sebastian Wiesner <sebastian@swsnr.de>
2023-08-10 09:55:01 +0200

    Use default wezterm hyperlink rules

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -130,24 +130,5 @@ return {
       mods = "NONE",
     },
   },
-  hyperlink_rules = {
-    -- Linkify things that look like URLs
-    -- This is actually the default if you don't specify any hyperlink_rules
-    {
-      regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
-      format = "$0",
-    },
-
-    -- linkify email addresses
-    {
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
-    },
-
-    -- file:// URI
-    {
-      regex = "\\bfile://\\S*\\b",
-      format = "$0",
-    },
-  },
+  hyperlink_rules = wezterm.default_hyperlink_rules(),
 }
