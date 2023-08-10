d796aedc52dd85c49459f0cd4084a91aabab93ab
Sebastian Wiesner <sebastian@swsnr.de>
2023-08-10 08:20:46 +0200

    Only override scheme if it changed

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -70,8 +70,10 @@ wezterm.on("window-config-reloaded", function(window, pane)
   local overrides = window:get_config_overrides() or {}
   local appearance = window:get_appearance()
   local scheme = scheme_for_appearance(appearance)
-  overrides.color_scheme = scheme
-  window:set_config_overrides(overrides)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
 end)
 
 return {
