6a2f5c14dda51c372b33c399738ab521f32e6321
Sebastian Wiesner <sebastian@swsnr.de>
2023-08-24 10:08:57 +0200

    Revert "On AC don't suspend automatically"
    This reverts commit 8d91ba8859fd22695dcd90b1cf23832a4fa595f7.

--- gnome/settings.py
+++ gnome/settings.py
@@ -34,8 +34,9 @@ SETTINGS = {
 
     # Power settings
     "org.gnome.settings-daemon.plugins.power": {
-        # Do not suspend automatically when on AC power
-        "sleep-inactive-ac-type": "nothing",
+        # Suspend after 30 minutes of inactivity when on AC power
+        "sleep-inactive-ac-timeout": 1800,
+        "sleep-inactive-ac-type": "suspend",
         # On battery suspend after five minutes already
         "sleep-inactive-battery-timeout": 300,
         "sleep-inactive-battery-type": "suspend",
