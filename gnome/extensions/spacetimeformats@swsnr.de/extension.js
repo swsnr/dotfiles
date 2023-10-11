7541da7625c0d945d9697a42ddded8faec725707
Sebastian Wiesner <sebastian@swsnr.de>
2023-10-11 09:25:02 +0200

    Add custom time formats to date menu button
    Do not add an additional indicator; instead fiddle with the date menu
    display to get our custom formats into the button.
    
    This makes the position of the clock more predictable.
    
    As a side-benefit we create a lot less types; in particular we also
    reuse the wall clock of the date menu button.

--- gnome/extensions/spacetimeformats@swsnr.de/extension.js
+++ gnome/extensions/spacetimeformats@swsnr.de/extension.js
@@ -14,102 +14,69 @@
 
 import St from 'gi://St';
 import GLib from 'gi://GLib';
-import GObject from 'gi://GObject'
-import Gio from 'gi://Gio';
 import Clutter from 'gi://Clutter';
-import GnomeDesktop from 'gi://GnomeDesktop';
 
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-
-const SpaceTimesIndicator = GObject.registerClass(
-  { GTypeName: "SpaceTimesIndicator" },
-  class SpaceTimesIndicator extends PanelMenu.Button {
-    _init({ name, settings }) {
-      super._init(5, name, true);
-
-      console.info("Listening for setting changes");
-      this._settings = settings;
-      this._settingsChangedId = this._settings.connect('changed', this._onSettingsChanged.bind(this));
-
-      console.info("Listening for wall clock ticks");
-      this._clock = new GnomeDesktop.WallClock()
-      this._clockNotifyId = this._clock.connect('notify::clock', this._updateLabel.bind(this));
-
-      this._label = new St.Label();
-      this._label.clutter_text.y_align = Clutter.ActorAlign.CENTER;
-      this.add_child(this._label);
-
-      this._updateLabel();
-      this._onSettingsChanged();
-    }
-
-    vfunc_event(event) {
-      if (this.menu &&
-        (event.type() == Clutter.EventType.TOUCH_BEGIN ||
-          event.type() == Clutter.EventType.BUTTON_PRESS)) {
-        this._open_uri();
-      }
-
-      return Clutter.EVENT_PROPAGATE;
-    }
-
-    _open_uri() {
-      const uri = this._get_uri_to_open();
-      console.info("Trying to open URI after click", uri);
-      if (!!uri) {
-        Gio.AppInfo.launch_default_for_uri_async(uri, null, null, null);
-      }
-    }
-
-    _get_uri_to_open() {
-      return this._settings.get_string('uri-to-open-on-click');
-    }
-
-    _onDestroy() {
-      this._label.destroy();
-      this._clock.disconnect(this._clockNotifyId);
-      this._clock.destroy();
-      this._settings.disconnect(this._settingsChangedId);
-      this._settings.destroy();
-      super._onDestroy();
-    }
-
-    _onSettingsChanged() {
-      // Enable clicking the indicator only of a URI was configured
-      const haveURI = !!this._get_uri_to_open();
-      console.info("Updating sensitive state of indicator", haveURI);
-      this.setSensitive(haveURI);
-    }
-
-    _updateLabel() {
-      const now = GLib.DateTime.new_now_utc();
-      this._label.set_text(now.format('%j/%H:%MZ CW %V'));
-    }
-  }
-);
 
 export default class SpaceTimeFormatsExtension extends Extension {
   constructor(metadata) {
     super(metadata);
-    this.indicator = null;
+    this._clockChangedId = null;
+    this._customLabel = null;
+  }
+
+  /**
+   * A simple accessor to the date menu wall clock.
+   */
+  get _clock() {
+    return Main.panel.statusArea.dateMenu._clock;
+  }
+
+  get _originalLabel() {
+    return Main.panel.statusArea.dateMenu.label_actor;
+  }
+
+  _updateLabel() {
+    if (this._customLabel !== null) {
+      const theirNow = this._clock.clock;
+      const ourNow = GLib.DateTime.new_now_utc().format('%j/%H:%MZ CW %V');
+      this._customLabel.set_text(`${theirNow} ${ourNow}`);
+    }
   }
 
   enable() {
-    if (this.indicator === null) {
-      console.log(`Extension ${this.uuid} enabled, creating indicator`);
-      const name = `${this.uuid} Indicator`;
-      this.indicator = new SpaceTimesIndicator({ name, settings: this.getSettings() });
-      Main.panel.addToStatusArea(name, this.indicator);
+    if (this._customLabel === null) {
+      console.log(`Extension ${this.uuid} enabled, creating new label`);
+      this._customLabel = new St.Label();
+      this._customLabel.clutter_text.y_align = Clutter.ActorAlign.CENTER;
+
+      // Hide the original label
+      this._originalLabel.set_width(0);
+      // Insert our custom label beneath the original label.  We need to use
+      // get_parent here because there are intermediate layout actors; the
+      // original label is not an immediate child of the date menu.
+      this._originalLabel.get_parent().insert_child_below(
+        this._customLabel, this._originalLabel)
+      this._updateLabel();
+    }
+    if (this._clockChangedId === null) {
+      console.log(`Extension ${this.uuid} enabled, connecting to clock`);
+      this._clockChangedId = this._clock.connect('notify::clock', this._updateLabel.bind(this));
     }
   }
 
   disable() {
-    if (this.indicator !== null) {
-      console.log(`Extension ${this.uuid} disabled, destroying indicator`);
-      this.indicator.destroy();
-      this.indicator = null;
+    if (this._clockChangedId !== null) {
+      console.log(`Extension ${this.uuid} disabled, disconnecting from clock`);
+      this._clock.disconnect(this._clockNotifyId);
+      this._clockChangedId = null;
+    }
+    if (this._customLabel !== null) {
+      console.log(`Extension ${this.uuid} disabled, destroying custom label`);
+      this._customLabel.destroy();
+      this._originalLabel.set_width(-1);
+      this._customLabel = null;
     }
   }
 }
