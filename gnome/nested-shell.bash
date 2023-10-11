7064614a2aed732515b1a887fa02b9dd3adf066f
Sebastian Wiesner <sebastian@swsnr.de>
2023-10-11 09:26:10 +0200

    Improve nested shell
    Enlarge the display, and enable all debugging messages.

--- gnome/nested-shell.bash
+++ gnome/nested-shell.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# See https://gjs.guide/extensions/development/debugging.html#running-a-nested-gnome-shell
+
 set -euo pipefail
+
+export G_MESSAGES_DEBUG=all
+export MUTTER_DEBUG_DUMMY_MODE_SPECS=1366x768
+
 exec dbus-run-session -- gnome-shell --nested --wayland
