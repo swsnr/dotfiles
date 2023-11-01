25c3ad8756d0dfea7241f62a5983f4de4ee1dd60
Sebastian Wiesner <sebastian@swsnr.de>
2023-11-01 12:23:54 +0100

    Upscale nested Gnome session

--- gnome/nested-shell.bash
+++ gnome/nested-shell.bash
@@ -4,6 +4,12 @@
 
 set -euo pipefail
 
-export MUTTER_DEBUG_DUMMY_MODE_SPECS=1366x768
+# See https://gitlab.gnome.org/GNOME/mutter/-/blob/main/src/backends/meta-monitor-manager-dummy.c
+# for documentation of these variables.
+#
+# If setting a scale > 1 the mode spec must at least be 1024x768 * scale, to provide
+# a minimum effective resolution of 1024x768.  Smaller scales will be ignored.
+export MUTTER_DEBUG_DUMMY_MODE_SPECS=2048x1536
+export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=2
 
 exec dbus-run-session -- gnome-shell --nested --wayland
