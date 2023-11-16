6830c7be0f3a2ba9d776479dc5fc08ad70f00455
Sebastian Wiesner <sebastian@swsnr.de>
2023-11-16 23:22:36 +0100

    Configure credential provider for cargo

--- install.bash
+++ install.bash
@@ -145,12 +145,14 @@ mkdir -p ~/.config/pipewire/pipewire.conf.d/
 clean-recursively ~/.config/pipewire/pipewire.conf.d/
 
 # Misc files
-mkdir -p ~/.config/{latexmk,restic,zim}
+mkdir -p ~/.config/{latexmk,restic,zim} ~/.cargo
 ln -fs "${DIR}/backup/linux.exclude" ~/.config/restic/linux.exclude
 ln -fs "${DIR}/latex/latexmkrc" ~/.config/latexmk/latexmkrc
 ln -fs "${DIR}/misc/XCompose" ~/.XCompose
 ln -fs "${DIR}/misc/gamemode.ini" ~/.config/gamemode.ini
 ln -fs "${DIR}/misc/zim-style.conf" ~/.config/zim/style.conf
+ln -fs "${DIR}/misc/cargo-config.toml" ~/.cargo/config.toml
+
 # Electron flags for all electron versions
 #
 # We use the following flags:
