43d8ad373f26ec14353467ac0005842c4b61dc97
Sebastian Wiesner <sebastian@swsnr.de>
2024-02-25 15:45:31 +0100

    Always check just the last three days

--- arch/check.bash
+++ arch/check.bash
@@ -15,31 +15,18 @@
 
 set -xeuo pipefail
 
-# Check the general state of the system
+# Check the general state of the system, in the last three days
 
-date_format='%Y-%m-%d %T'
-cache_dir="${XDG_CACHE_DIR:-${HOME}/.cache}/swsnr/dotfiles"
-last_check_file="${cache_dir}/last-check"
-
-mkdir -p "${cache_dir}"
-if [[ -e "${last_check_file}" ]]; then
-    last_check_timestamp="$(<"${last_check_file}")"
-else
-    # Start a week ago
-    two_weeks_s=1209600
-    now_s="$(date --utc '+%s')"
-    two_weeks_ago="$((now_s - two_weeks_s))"
-    # Convert to timestamp and cut of timezone offset. It's zero anyways.
-    # Then replace T with a space, because that's the format systemd wants
-    last_check_timestamp="$(date -d@"${two_weeks_ago}" +"${date_format}")"
-fi
-
+# Three days
+two_weeks_s=259200
+now_s="$(date --utc '+%s')"
+two_weeks_ago="$((now_s - two_weeks_s))"
+# Convert to timestamp and cut of timezone offset. It's zero anyways.
+# Then replace T with a space, because that's the format systemd wants
+last_check_timestamp="$(date -d@"${two_weeks_ago}" +'%Y-%m-%d %T')"
 echo "Checking events since ${last_check_timestamp}"
 
 # List failed units, new coredumps, and journalctl
 systemctl --failed
 coredumpctl --no-pager --since="${last_check_timestamp}" list || true
 journalctl --no-pager -p3 --since="${last_check_timestamp}"
-
-# Remember date of last check
-date +"${date_format}" >"${last_check_file}"
